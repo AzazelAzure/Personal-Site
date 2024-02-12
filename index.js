ac59a2262679d965403ec6238159928d9021a10e
Patrick Proctor <etusnproctor@gmail.com>
2024-02-12 14:40:38 -0600

    Prelim testing and build

--- index.js
+++ index.js
@@ -4,16 +4,22 @@ import bodyParser from 'body-parser';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
+
 // Declare Constants
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const port = 8000;
 
 // Middleware
-
+app.use(express.static('public'));
 
 // Site handlers
 app.get('/', (req, res)=>{
-    app.render(__dirname + 'views/index.ejs');
+    res.render(__dirname + '/views/index.ejs');
+})
+
+app.listen(8000, ()=>{
+    console.log(`Listening on port ${port}`);
 })
 
 // Functions
