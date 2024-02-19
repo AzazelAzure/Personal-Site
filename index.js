1a84be0a78248ddfa80546a0134d31780b5812ca
Patrick Proctor <etusnproctor@gmail.com>
2024-02-19 13:12:39 -0600

    Added logging and email functionality

--- index.js
+++ index.js
@@ -5,18 +5,60 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
 import https from 'https';
+import nodemailer from 'nodemailer';
+import winston from 'winston';
 
-
-// Declare Constants
+// App constants
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+//const port = 3000;
+
+//HTTPs Constants
 const httpsPort = 443;
-const hostname = 'azazelazure.com'
+const hostname = process.env.IP_ADDRESS
+
 const options = {
-    key: fs.readFileSync('My Key.pem'), 
-    cert: fs.readFileSync('csr.pem')
+    key: fs.readFileSync('private.key'), 
+    cert: fs.readFileSync('certificate.crt')
 }
+
 const httpsServer = https.createServer(options, app);
+
+//Email forwarding constants
+const password = process.env.EMAIL_PASSWORD
+const emailUsername = process.env.EMAIL_USER
+
+const tranporter = nodemailer.createTransport({
+    host:'mail.privateemail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'contactme@azazelazure.com',
+        pass: 'g0fucky0ur$3lf'
+    }
+})
+
+
+
+//Logging Constants
+const { combine, timestamp, prettyPrint, label } = winston.format;
+
+const logger = winston.createLogger({
+    format: combine(
+        timestamp(),
+        prettyPrint(),
+        winston.format.splat(),
+        label({label:'Time since last log'}),
+        winston.format.ms(),
+    ),
+    transports: [
+        new winston.transports.File({filename: 'combined.log'}),
+        new winston.transports.File({filename: 'error.log', level: 'error'}),
+    ]
+});
+
+
 
 // Middleware
 app.use(express.static('public'));
@@ -39,10 +81,51 @@ app.get('/beyond', (req, res)=>{
     res.redirect('https://www.beyondmed.org');
 })
 
+app.post('/email', (req, res)=>{
+    let message = {
+        from: 'contactme@azazelazure.com',
+        to: 'contactme@azazelazure.com',
+        sender: req.body.email,
+        subject: "Message from " + req.body.name,
+        text: req.body.text,
+
+    };
+    if (req.xhr || req.headers.accept.indexOf('json') > -1){
+       tranporter.sendMail(message, (error, info)=>{
+        if (error){
+            logger.log({
+                level: 'error',
+                message: `${error}`
+            });
+            res.status(500).json({success: false, message: 'Error sending message'});
+        } else{
+            logger.log({
+                level: 'info',
+                message: 'Email sent: ' + info.response
+            })
+            res.status(200).json({success: true})    
+        }
+    }) 
+    } else{
+        res.render(__dirname + '/views/contact.ejs');
+    }
+    
+    
+})
+
 // app.listen(3000, ()=>{
-//     console.log(`Listening on port ${port}`);
+//     console.log(`Listening on port ${port}`)
+//     logger.log({
+//         level: 'info',
+//         message: `Server started`
+//     });
 // })
 
-httpsServer.listen(httpsPort, hostname);
+httpsServer.listen(httpsPort, hostname, ()=>{
+    logger.log({
+        level: 'info',
+        message: 'Server started'
+    })
+});
 
 // Functions
---
+++ public/JS/site.js
@@ -0,0 +1,25 @@
+// import { error } from "winston";
+
+$(document).ready(function() { 
+    $('#contact').submit(function(event) { 
+        event.preventDefault(); 
+
+        $.ajax({
+            type: 'POST',
+            url: '/email', 
+            data: $(this).serialize(), 
+            success: function(response) { // Handle response
+                console.log(`Success reponse: ${response}`)
+                if (response.success) {
+                    $('#liveToast').toast('show')
+                } else {
+                    alert('There was a problem submitting the form.');
+                }
+            },
+            error: function() { 
+                console.log(`there was and error ${error}`)
+                alert('There was a problem submitting the form.'); 
+            }
+        });
+    });
+});
