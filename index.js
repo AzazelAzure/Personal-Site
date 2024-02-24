b1ebe8b3e17b8b6daa569a7510046f4c0dfd07fb
EC2 Default User <ec2-user@ip-172-31-6-147.us-east-2.compute.internal>
2024-02-24 09:49:35 +0000

    Add email functionality.

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ import { dirname } from 'path';
 import { fileURLToPath, format } from 'url';
 import fs from 'fs';
 import https from 'https';
-import nodemailer from 'nodemailer';
 import winston from 'winston';
+import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
+import 'dotenv/config'
+
 
 // App constants
 const app = express();
@@ -17,30 +19,23 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 //HTTPs Constants
 const httpsPort = 443;
 const hostname = process.env.IP_ADDRESS
+const key = process.env.SSL_KEY
+const cert = process.env.SSL_CERT
+const ca = process.env.SSL_CA
 
 const options = {
-    key: fs.readFileSync('private.key'), 
-    cert: fs.readFileSync('certificate.crt'),
-    ca: fs.readFileSync('www_azazelazure_com.ca-bundle')
+    key: fs.readFileSync(key), 
+    cert: fs.readFileSync(cert),
+    ca: fs.readFileSync(ca)
 }
 
 const httpsServer = https.createServer(options, app);
 
 //Email forwarding constants
-const password = process.env.EMAIL_PASSWORD
+const region = process.env.REGION
 const emailUsername = process.env.EMAIL_USER
 
-const tranporter = nodemailer.createTransport({
-    host:'mail.privateemail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        user: emailUsername,
-        pass: password
-    }
-})
-
-
+const ses = new SESClient({ region: region });
 
 //Logging Constants
 const { combine, timestamp, prettyPrint, label } = winston.format;
@@ -83,36 +78,34 @@ app.get('/beyond', (req, res)=>{
     res.redirect('https://www.beyondmed.org');
 })
 
-app.post('/email', (req, res)=>{
-    let message = {
-        from: emailUsername,
-        to: emailUsername,
-        subject: "Message from " + req.body.name + " " + req.body.email,
-        text: req.body.text 
-
-    };
-    if (req.xhr || req.headers.accept.indexOf('json') > -1){
-       tranporter.sendMail(message, (error, info)=>{
-        if (error){
-            logger.log({
-                level: 'error',
-                message: `${error}`
-            });
-            res.status(500).json({success: false, message: 'Error sending message'});
-        } else{
-            logger.log({
-                level: 'info',
-                message: 'Email sent: ' + info.response
-            })
-            res.status(200).json({success: true})    
-        }
-    }) 
-    } else{
-        res.render(__dirname + '/views/contact.ejs');
-    }
-    
-    
-})
+app.post('/email', async (req, res)=>{
+	const params = {
+		Source: emailUsername,
+		Destination: {ToAddresses: [emailUsername]},
+		Message:{
+			Subject:{
+				Data:'Message from ' + req.body.name, 
+				Charset: 'UTF-8',
+			},
+			Body:{
+				Text:{
+					Data:req.body.text + ' sent from: ' + req.body.email, 
+					Charset: 'UTF-8',
+				},
+			},
+		},
+	};
+	
+	try{
+		const command = new SendEmailCommand(params);
+		const info = await ses.send(command);
+		logger.log({level: 'info', message: 'Email sent: ' + info.response});
+		res.status(200).json({success: true});
+	} catch (error) {
+		logger.log({level: 'error', message: `${error}`});
+		res.status(500).json({success: false, message: 'Error sending message'});
+	}
+});
 
 // app.listen(3000, ()=>{
 //     console.log(`Listening on port ${port}`)
@@ -129,4 +122,3 @@ httpsServer.listen(httpsPort, hostname, ()=>{
     })
 });
 
-// Functions
